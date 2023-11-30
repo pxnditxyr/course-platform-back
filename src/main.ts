1d238ade96aa73082580a5277d84c7da1b11da8a
Yuki xD <pxnditxyr@gmail.com>
2023-11-30 13:19:53 -0400

    Initialized a new project 'Landing Page Courses, News, etc. Kantuta' Backend 🚀🌟🔧

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core'
+import type { NestExpressApplication } from '@nestjs/platform-express'
+import { AppModule } from './app.module'
+import { Logger, ValidationPipe } from '@nestjs/common'
+
+async function bootstrap() {
+  const logger = new Logger( 'Main' )
+  const app = await NestFactory.create<NestExpressApplication>( AppModule )
+
+  app.enableCors()
+  app.useGlobalPipes( new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }) )
+
+  app.setGlobalPrefix( 'api' )
+
+  await app.listen( process.env.PORT || 3000 )
+  logger.verbose( `Application is running on: ${ await app.getUrl() }` )
+}
+bootstrap()
